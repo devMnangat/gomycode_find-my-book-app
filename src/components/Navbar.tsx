8b72b96a6f6fa8659cc1c3cacfb5ced6ded7a534
Emmanuel Mnangat <emnangat99@gmail.com>
2024-07-13 02:28:49 +0300

    chore: add library link to navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,13 +3,19 @@
 import React from "react";
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
+import { IoLibrarySharp } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <nav className="w-full h-fit flex flex-col md:flex-row justify-between border-black bg-theme-primary text-theme-secondary text-xl p-4 md:p-6">
-      <Link href={"/"}>
-        <FaHome />
-      </Link>
+      <div className="gap-3 md:flex md:gap-4">
+        <Link href={"/"}>
+          <FaHome />
+        </Link>
+        <Link href={"/save"}>
+          <IoLibrarySharp />
+        </Link>
+      </div>
       <ul className="w-full flex-col md:w-fit md:flex-row flex gap-4  items-end">
         <li className=" hover:text-theme-natural">
           <Link href={"/dashboard"}>Dashboard</Link>
@@ -17,8 +23,14 @@ const Navbar = () => {
         <li className=" hover:text-theme-natural">
           <Link href={"/books"}>About</Link>
         </li>
-        <li >
-          <Link className="hover:underline" href={"/login"}>Login</Link>/ <Link className="hover:underline" href={"/register"}>Register</Link>
+        <li>
+          <Link className="hover:underline" href={"/login"}>
+            Login
+          </Link>
+          /{" "}
+          <Link className="hover:underline" href={"/register"}>
+            Register
+          </Link>
         </li>
       </ul>
     </nav>
