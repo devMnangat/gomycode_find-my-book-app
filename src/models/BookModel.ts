c9903fcdd21d13df5bbb30567e5f87285b429c3d
Emmanuel Mnangat <emnangat99@gmail.com>
2024-07-14 17:46:56 +0300

    chore: additional styling and debug

--- src/models/BookModel.ts
+++ src/models/BookModel.ts
@@ -1,22 +1,19 @@
 import { Book } from "@/types/book";
 import { Schema, model, models } from "mongoose";
 
-
 const bookSchema: Schema<Book> = new Schema({
-    title: { type: String, required: true },
-    authors: { type: [String], required: true },
-    subtitle: { type: String, required: true },
-    imageLinks: {
-        smallThumbnail: { type: String, required: true },
-        thumbnail: { type: String, required: true },
-    },
-    previewLink: { type: String, required: true },
-    language: { type: String, required: true },
-    pageCount: { type: Number, required: true },
+  title: { type: String, required: true },
+  authors: { type: [String], required: true },
+  subtitle: { type: String, required: true },
+  imageLinks: {
+    smallThumbnail: { type: String, required: true },
+    thumbnail: { type: String, required: true },
+  },
+  previewLink: { type: String, required: true },
+  language: { type: String, required: true },
+  pageCount: { type: Number, required: true },
 });
 
-
-const BookModel =
-  models.Book || model("Book", bookSchema);
+const BookModel = models.Book || model("Book", bookSchema);
 
 export default BookModel;
