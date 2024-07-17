3e56aa4d0d032fba101023b5055014fb39ce2479
Emmanuel Mnangat <emnangat99@gmail.com>
2024-07-17 15:44:39 +0300

    chore: use the logout component here after importing

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,18 @@ import React from "react";
 import Link from "next/link";
 import { FaHome } from "react-icons/fa";
 import { IoLibrarySharp } from "react-icons/io5";
+import { getServerAuthSession } from "@/server/auth";
+import LogoutButton from "./LogoutButton";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const session = await getServerAuthSession();
   return (
     <nav className="w-full h-fit flex flex-col md:flex-row justify-between border-black bg-theme-primary text-theme-secondary text-xl p-4 md:p-6">
       <div className="gap-3 md:flex md:gap-4">
-        <Link href={"/"}>
+        <Link href={"/"} className="text-lg md:text-xl">
           <FaHome />
         </Link>
-        <Link href={"/save"}>
+        <Link href={"/save"} className="text-lg md:text-xl">
           <IoLibrarySharp />
         </Link>
       </div>
@@ -23,15 +26,23 @@ const Navbar = () => {
         <li className=" hover:text-theme-natural">
           <Link href={"/books"}>About</Link>
         </li>
-        <li>
-          <Link className="hover:underline" href={"/login"}>
-            Login
-          </Link>
-          /{" "}
-          <Link className="hover:underline" href={"/register"}>
-            Register
-          </Link>
-        </li>
+        {!session?.user ? (
+          <li>
+            <Link className="hover:underline" href={"/login"}>
+              Login
+            </Link>
+            /{" "}
+            <Link className="hover:underline" href={"/register"}>
+              Register
+            </Link>
+          </li>
+        ) : (
+          <li>
+            <LogoutButton>
+              <h1 className="hover:underline">Logout</h1>
+            </LogoutButton>
+          </li>
+        )}
       </ul>
     </nav>
   );
