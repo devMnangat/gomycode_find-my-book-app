da3c74dd53c3cae38e27b25a1c97ee419d555164
Emmanuel Mnangat <emnangat99@gmail.com>
2024-07-19 10:22:21 +0300

    chore: additional styling

--- src/app/save/page.tsx
+++ src/app/save/page.tsx
@@ -81,7 +81,7 @@ const Library = () => {
   }
 
   return (
-    <div className="container mx-auto px-4 py-10">
+    <div className="container mx-auto px-4 py-10 overflow-y-scroll h-screen">
       <h1 className="text-4xl font-bold mb-8 text-center">My Library</h1>
       {books.length === 0 ? (
         <p className="text-center">No books saved yet.</p>
